df8c191a3b595418acc39d6a7333bc2f1691aa3a
artym125 <otapik125@gmail.com>
2024-02-08 20:44:33 +0200

    Initial Commit

---
+++ ManualLanguageСhange/ManualLanguage_hangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManualLanguage_hangeApp.swift
+//  ManualLanguageСhange
+//
+//  Created by Ostap Artym on 08.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ManualLanguage_hangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
