a47d02567f0bc03ef87fd021aac029f9693eebd5
artym125 <otapik125@gmail.com>
2024-02-08 21:07:42 +0200

    Basic example of changing the language

--- ManualLanguageСhange/ContentView.swift
+++ ManualLanguageСhange/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @AppStorage("language")
+    private var language = LocalizationService.shared.language
+    //Додаємо значення через @AppStorage
+    
+    var title: String = "MainPageTitle"
+    //"MainPageTitle" - ключ з локалізації
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 15) {
+            
+            Text(title.localized(language))
+            
+            //.localized(language) - добавляємо для String
+            
+            Button {
+                LocalizationService.shared.language = .ukrainian
+            } label: {
+                Text("Українська")
+                    .font(.title2.bold())
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.black.opacity(0.50), in: RoundedRectangle(cornerRadius: 15))
+            }
+            Button {
+                LocalizationService.shared.language = .english_us
+            } label: {
+                Text("English")
+                    .font(.title2.bold())
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.black.opacity(0.50), in: RoundedRectangle(cornerRadius: 15))
+            }
         }
         .padding()
     }
---
+++ ManualLanguageСhange/Language.swift
@@ -0,0 +1,64 @@
+//
+//  Language.swift
+//  ManualLanguageСhange
+//
+//  Created by Ostap Artym on 08.02.2024.
+//
+
+import Foundation
+
+enum Language: String {
+    case english_us = "en"
+    case ukrainian = "uk"
+}
+
+extension String {
+
+    func localized(_ language: Language) -> String {
+        let path = Bundle.main.path(forResource: language.rawValue, ofType: "lproj")
+        let bundle: Bundle
+        if let path = path {
+            bundle = Bundle(path: path) ?? .main
+        } else {
+            bundle = .main
+        }
+        return localized(bundle: bundle)
+    }
+
+    func localized(_ language: Language, args arguments: CVarArg...) -> String {
+        let path = Bundle.main.path(forResource: language.rawValue, ofType: "lproj")
+        let bundle: Bundle
+        if let path = path {
+            bundle = Bundle(path: path) ?? .main
+        } else {
+            bundle = .main
+        }
+        return String(format: localized(bundle: bundle), arguments: arguments)
+    }
+
+    private func localized(bundle: Bundle) -> String {
+        return NSLocalizedString(self, bundle: bundle, value: "", comment: "")
+    }
+}
+
+class LocalizationService {
+    
+    static let shared = LocalizationService()
+    static let changedLanguage = Notification.Name("changedLanguage")
+
+    private init() {}
+    
+    var language: Language {
+        get {
+            guard let languageString = UserDefaults.standard.string(forKey: "language") else {
+                return .english_us
+            }
+            return Language(rawValue: languageString) ?? .english_us // - дефолтне значення мови у програмі
+        } set {
+            if newValue != language {
+                UserDefaults.standard.setValue(newValue.rawValue, forKey: "language")
+                NotificationCenter.default.post(name: LocalizationService.changedLanguage, object: nil)
+            }
+        }
+    }
+}
